384529936311c85980b430c7ecf6f8547e05aac6
Miłosz <miloszz.krakowiak@gmail.com>
2026-04-19 15:23:15 +0200

    Fixed column alias

--- server.js
+++ server.js
@@ -346,7 +346,7 @@ ORDER BY u.points DESC, total_time_spent  ASC, u.email ASC LIMIT 10`
                         const data = JSON.parse(body);
                         connection = await pool.getConnection();
                         const [rows] = await connection.execute(
-                            `SELECT CONCAT(SUBSTR(u.email,1,3),'<adres>@poczta.pl') as "email", sum(q.points_awarded ), sum(uqa.time_spent) as 'timeSpentTotal' FROM users u
+                            `SELECT CONCAT(SUBSTR(u.email,1,3),'<adres>@poczta.pl') as "email", sum(q.points_awarded ) as "points" , sum(uqa.time_spent) as 'timeSpentTotal' FROM users u
 INNER JOIN user_questions_answered uqa ON uqa.user_id = u.id 
 INNER JOIN questions q ON q.id = uqa.question_id 
 GROUP BY u.email, uqa.correct, q.subject 
